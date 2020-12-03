9c87d875c0438356d31f30a2d724e111fbf49169
menachem m margolis <margolismendel5728@gmail.com>
2020-12-03 23:17:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,7 +146,7 @@ def shoe_size(player_name)
 end
 def team_colors (team_names)
  game_hash.each do |keys,values|
-   return values[:colors]
+   return values[:colors] if values ==values[:team_names]
  end
 
 end
